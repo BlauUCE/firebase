e44184784bd51daefb4e77f8d8408614df3dbfae
BlauUCE <nb_correo@hotmail.com>
2020-04-16 23:05:44 -0500

    listado de database firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+//firebase
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+//import 'firebase/firestore'
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +12,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'FirebaseCrud';
+
+  items: Observable<any[]>;
+  constructor(firestore: AngularFirestore) {
+    this.items = firestore.collection('items').valueChanges();
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,9 +2,9 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
+/* export const environment = {
   production: false
-};
+}; */
 
 /*
  * For easier debugging in development mode, you can import the following file
@@ -14,3 +14,17 @@ export const environment = {
  * on performance if an error is thrown.
  */
 // import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAN9W2EUKYMriWU5UI7EUftJs3U6Ig7bU8",
+    authDomain: "ngfirebaseproy-5efbe.firebaseapp.com",
+    databaseURL: "https://ngfirebaseproy-5efbe.firebaseio.com",
+    projectId: "ngfirebaseproy-5efbe",
+    storageBucket: "ngfirebaseproy-5efbe.appspot.com",
+    messagingSenderId: "538672753910",
+    appId: "1:538672753910:web:edf2e21ae5af58085fd0d7",
+    measurementId: "G-VGRRCNELKL"
+  }
+};
